9802a812552861763a7323bebcfe2c06943c5cd6
miguelturmo <125253347+miguelturmo@users.noreply.github.com>
2024-11-18 10:29:10 +0100

    .

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -9,12 +9,13 @@ public class Damage : MonoBehaviour
 
     private float baseDamage = 50;
     private float totalDamage = 0;
-    
+    private float multiplier = 1;
     private ColorPrecision colorPrecision;
-
+    private ButtonManager buttonManager;
     private void Start()
     {
         colorPrecision = GameObject.FindObjectOfType<ColorPrecision>();
+        buttonManager = GameObject.FindObjectOfType<ButtonManager>();
     }
 
     private float CalculateDamage() 
@@ -22,29 +23,59 @@ public class Damage : MonoBehaviour
         float precision = colorPrecision.GetPrecision();
         ScoreManager.instance.SetScore((int)precision);
 
-        float damage = 0;
-        switch (precision) 
+        if(buttonManager.atack)
         {
-            case < 50:
-                damage = 0;
-                break;
-            case < 60:
-                damage = baseDamage - (baseDamage * 0.1f);
-                break;
-            case < 80:
-                damage = baseDamage + (baseDamage * 0.15f);
-                baseDamage += 5;
-                break;
-            case < 98:
-                damage = baseDamage + (baseDamage * 0.35f);
-                baseDamage += 10;
-                break;    
-            case <= 100:
-                damage = baseDamage + (baseDamage * 1.0f);
-                baseDamage += 15;
-                break;     
+            float damage = 0;
+            switch (precision)
+            {
+                case < 50:
+                    damage = 0;
+                    break;
+                case < 60:
+                    damage = baseDamage - (baseDamage * 0.1f);
+                    break;
+                case < 80:
+                    damage = baseDamage + (baseDamage * 0.15f);
+                    baseDamage += 5;
+                    break;
+                case < 98:
+                    damage = baseDamage + (baseDamage * 0.35f);
+                    baseDamage += 10;
+                    break;
+                case <= 100:
+                    damage = baseDamage + (baseDamage * 1.0f);
+                    baseDamage += 15;
+                    break;
+            }
+            return damage;
         }
-        return damage;
+        if(buttonManager.defense)
+        {
+            float defense = 0;
+            switch (precision)
+            {
+                case < 50:
+                    defense = 0;
+                    break;
+                case < 60:
+                    defense = baseDamage - (baseDamage * 0.1f);
+                    break;
+                case < 80:
+                    defense = baseDamage + (baseDamage * 0.15f);
+                    baseDamage += 5;
+                    break;
+                case < 98:
+                    defense = baseDamage + (baseDamage * 0.35f);
+                    baseDamage += 10;
+                    break;
+                case <= 100:
+                    defense = baseDamage + (baseDamage * 1.0f);
+                    baseDamage += 15;
+                    break;
+            }
+            return defense;
+        }
+       
     }
 
     public void SetDamage()
