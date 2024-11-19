261be5ebba09e110c654d4309e0b5726b2bce884
guillemal <guillem.alqueza@estudiantat.upc.edu>
2024-11-19 22:32:21 +0100

    Delete script

--- Assets/Scripts/ColorsMagic.cs
+++ Assets/Scripts/ColorsMagic.cs
@@ -5,21 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class ColorsMagic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ChangeToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
-
 }
